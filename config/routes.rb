516945647e03fb9560475c8688ee29026381e7b7
Sarah Hammond <s.hammond@berkeley.edu>
2017-11-28 01:27:25 -0800

    fixed routes/forms for orders/new

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  root "orders#create"
+  get '/', to: "orders#new", as: 'orders'
+  post '/', to: "orders#create", as: 'create'
   resources :order
   resources :meal
   resources :employee
-  get 'meal/all', to: 'meal#list', as meal_list
-  get 'order/all', to: 'order#list', as order_list
+  get 'meal/all', to: 'meal#list', as: 'meal_list'
+  get 'order/all', to: 'order#list', as: 'order_list'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
